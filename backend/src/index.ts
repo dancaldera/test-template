d811c7a736b65919beac3a8a2500453702a7ea0b
Daniel <bdcaldera@gmail.com>
2024-03-04 20:06:33 -0600

    feat: finished backend template

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const prisma = new PrismaClient()
+const app: Express = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('API IS WORKING')
+})
+
+app.get('/users', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
